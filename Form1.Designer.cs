1bd008027261d3f9c19762bdd2256e26142d5904
StoriedOrb <57195998+StoriedOrb@users.noreply.github.com>
2019-12-18 09:39:31 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ScreenResolution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labeltime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Стрелки - движение.\r\nСтрелки + Shift - расширение.\r\nСтрелки + Ctrl - сужение.\r\nSp" +
+    "ace - чтение координат.\r\nКолесо мыши - прозрачность.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Silver;
+            this.label2.Location = new System.Drawing.Point(-1, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "YYYYY";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Silver;
+            this.label3.Location = new System.Drawing.Point(-1, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "XXXXX";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labeltime
+            // 
+            this.labeltime.AutoSize = true;
+            this.labeltime.BackColor = System.Drawing.Color.Silver;
+            this.labeltime.Location = new System.Drawing.Point(-1, 78);
+            this.labeltime.Name = "labeltime";
+            this.labeltime.Size = new System.Drawing.Size(0, 13);
+            this.labeltime.TabIndex = 3;
+            this.labeltime.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(512, 341);
+            this.Controls.Add(this.labeltime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labeltime;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenResolution
+{
+    public partial class Form1 : Form
+    {
+        public bool space = true;
+        public int t1 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            this.MouseWheel += new MouseEventHandler(Form1_MouseWheel);
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (space == true)
+            {
+                label2.Text = "Координата У: " + (e.Y).ToString();
+                label3.Text = "Координата Х: " + (e.X).ToString();
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)     //////////// SPACE
+            {
+                if (space == false)
+                {
+                    space = true;
+                }
+                else
+                {
+                    space = false;
+                }
+            }
+            if (e.KeyCode == Keys.Up && e.Shift)       //////////// UP
+            {
+                this.Height += 4;
+                this.Top -= 4;
+            }
+            else
+            {
+                if (e.KeyCode == Keys.Up && e.Control)
+                {
+                    this.Height -= 4;
+                }
+                else
+                {
+                    if (e.KeyCode == Keys.Up)
+                    {
+                        this.Top -= 4;
+                    }
+                }
+            }
+
+            if (e.KeyCode == Keys.Left && e.Shift)      //////////// LEFT
+            {
+                this.Width += 4;
+                this.Left -= 4;
+            }
+            else
+            {
+                if (e.KeyCode == Keys.Left && e.Control)
+                {
+                    this.Width -= 4;
+                }
+                else
+                {
+                    if (e.KeyCode == Keys.Left)
+                    {
+                        this.Left -= 4;
+                    }
+                }
+            }
+
+            if (e.KeyCode == Keys.Down && e.Shift)      //////////// DOWN
+            {
+                this.Height += 4;
+            }
+            else
+            {
+                if (e.KeyCode == Keys.Down && e.Control)
+                {
+                    this.Height -= 4;
+                    this.Top += 4;
+                }
+                else
+                {
+                    if (e.KeyCode == Keys.Down)
+                    {
+                        this.Top += 4;
+                    }
+                }
+            }
+
+            if (e.KeyCode == Keys.Right && e.Shift)     //////////// RIGHT
+            {
+                this.Width += 4;
+            }
+            else
+            {
+                if (e.KeyCode == Keys.Right && e.Control)
+                {
+                    this.Width -= 4;
+                    this.Left += 4;
+                }
+                else
+                {
+                    if (e.KeyCode == Keys.Right)
+                    {
+                        this.Left += 4;
+                    }
+                }
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)     //////////// t1
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                t1 = 1;
+                timer1.Start();
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                t1 = 2;
+                timer1.Start();
+            }
+            if (e.Button == MouseButtons.Middle)
+            {
+                Random rnd = new Random();
+                this.BackColor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+            }
+
+        }
+        private void Form1_MouseWheel(object sender, MouseEventArgs b)     //////////// OPACITY
+        {
+            if (b.Delta > 0)
+            {
+                Opacity += 0.009;
+            }
+            else
+            {
+                Opacity -= 0.009;
+                if (Opacity < 0.05)
+                {
+                    Opacity = 1;
+                }
+            }
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)    /////DATE AND TIME
+        {
+            if (t1 == 1)
+                this.Text = DateTime.Now.ToString("HH:mm:ss");
+            if (t1 == 2)
+                this.Text = DateTime.Now.ToString("dd MMMM yyyy");
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
